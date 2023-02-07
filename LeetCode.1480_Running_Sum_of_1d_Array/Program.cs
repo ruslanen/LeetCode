f1806e08ce0cbb1844a0c94342d86b718ee2fe15
Ruslan Khaliullin <ruslan68a@gmail.com>
2023-02-07 07:54:11 +0300

    LeetCode75 - 1480, 704, 724.

---
+++ LeetCode.1480_Running_Sum_of_1d_Array/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var data = new int[] {1, 2, 3, 4};
+        var solution = new Solution();
+        var result = solution.RunningSum(data);
+        Console.WriteLine(string.Join(", ", result));
+    }
+}
+
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        var result = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                result[i] += nums[j];
+            }
+        }
+
+        return result;
+    } 
+}
+
+
---
+++ LeetCode.724_Find_Pivot_Index/Program.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var nums = new int[] {1, 7, 3, 6, 5, 6};
+        var solution = new Solution();
+        var result = solution.PivotIndex(nums);
+        Console.WriteLine(result);
+    }
+}
+
+public class Solution
+{
+    public int PivotIndex(int[] nums)
+    {
+        var index = -1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var left = 0;
+            for (int j = 0; j < i; j++)
+            {
+                left += nums[j];
+            }
+
+            var right = 0;
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                right += nums[j];
+            }
+
+            if (left == right)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+}
