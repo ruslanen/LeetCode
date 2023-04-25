351c331251d97d0370393100f20472ac1664f58a
Ruslan Khaliullin <ruslan68a@gmail.com>
2023-04-25 18:45:33 +0300

    347. Top K Frequent Elements.

---
+++ LeetCode._347_Top_K_Frequent_Elements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode._347_Top_K_Frequent_Elements;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.TopKFrequent(new[] {1, 1, 1, 2, 2, 3}, 2);
+    }
+}
---
+++ LeetCode._347_Top_K_Frequent_Elements/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._347_Top_K_Frequent_Elements;
+
+public class Solution
+{
+    // O(n log n)
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var dictionary = new Dictionary<int, int>();
+        // Заполнение словаря - O(n)
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dictionary.TryGetValue(nums[i], out var value))
+            {
+                dictionary[nums[i]] = ++value;
+            }
+            else
+            {
+                dictionary[nums[i]] = 1;
+            }
+        }
+
+        // OrderByDescending использует QuickSort с O(n log n)
+        // Take выбирает заданное количество элементов, что занимает O(k)
+        // ToArray создает новый массив, что занимает O(k)
+        return dictionary.OrderByDescending(x => x.Value)
+            .Select(x => x.Key)
+            .Take(k)
+            .ToArray();
+    }
+}
