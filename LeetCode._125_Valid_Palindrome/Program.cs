c2665f5e422784eb6d6e4a380e571f3500b6c9ba
Ruslan Khaliullin <ruslan68a@gmail.com>
2023-05-06 15:41:40 +0300

    125. Valid Palindrome.

---
+++ LeetCode._125_Valid_Palindrome/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode._125_Valid_Palindrome;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.IsPalindrome(" ");
+        var result2 = solution.IsPalindrome("A man, a plan, a canal: Panama");
+        var result3 = solution.IsPalindrome("race a car");
+        var result4 = solution.IsPalindrome("0P");
+    }
+}
---
+++ LeetCode._125_Valid_Palindrome/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace LeetCode._125_Valid_Palindrome;
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        var stringBuilder = new StringBuilder();
+        foreach (var symbol in s)
+        {
+            if (char.IsLetter(symbol))
+            {
+                stringBuilder.Append(char.ToLowerInvariant(symbol));
+            }
+            else if (char.IsDigit(symbol))
+            {
+                stringBuilder.Append(symbol);
+            }
+        }
+
+        var result = stringBuilder.ToString();
+        return result == string.Join("", result.ToCharArray().Reverse());
+    }
+}
