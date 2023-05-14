e73a52cf310f5e2d2a120b3b5186e99a7249fea4
Ruslan Khaliullin <ruslan68a@gmail.com>
2023-05-14 10:07:55 +0300

    167. Two Sum II - Input Array Is Sorted.

---
+++ LeetCode._167_Two_Sum_II_Input_Array_Is_Sorted/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode._167_Two_Sum_II_Input_Array_Is_Sorted;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.TwoSum(new[] {2, 7, 11, 15}, 9); // [0, 1]
+        var result2 = solution.TwoSum(new[] {2, 3, 4}, 6); // [1, 3]
+        var result3 = solution.TwoSum(new[] {-1, 0}, -1); // [0, 1]
+        var result4 = solution.TwoSum(new[] {0, 0, 3, 4}, 0); // [1, 2]
+        var result5 = solution.TwoSum(new[] {0, 0, 1, 1, 4, 5}, 6); // [3, 6]
+    }
+}
---
+++ LeetCode._167_Two_Sum_II_Input_Array_Is_Sorted/Solution.cs
@@ -0,0 +1,89 @@
+﻿namespace LeetCode._167_Two_Sum_II_Input_Array_Is_Sorted;
+
+public class Solution
+{
+    // OK
+    // O(n^2)
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var numberToFind = target - numbers[i];
+            for (int j = i + 1; j < numbers.Length; j++)
+            {
+                if (numberToFind == numbers[j])
+                {
+                    return new[] {i + 1, j + 1};
+                }
+            }
+        }
+
+        throw new Exception("Unreachable code");
+    }
+
+    // O(nlog n)
+    // Лучший способ - бинарный поиск
+    public int[] TwoSumV3(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            // Берем следующий индекс (заранее знаем, что нельзя использовать один и тот же элемент)
+            var start = i + 1;
+            // Берем индекс последнего элемента
+            var end = numbers.Length - 1;
+            // Вычисляем значение, которое требуется найти в случае,
+            // если одно и двух будет равно текущему numbers[i]
+            var sum = target - numbers[i];
+            // Так как массив отсортирован заранее, то можно выполнить бинарный поиск
+            while (start <= end)
+            {
+                // Находим число посередине
+                var mid = start + (end - start) / 2;
+                if (numbers[mid] == sum)
+                {
+                    return new[] {i + 1, mid + 1};
+                }
+
+                if (numbers[mid] > sum)
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+        }
+
+        throw new Exception("Unreachable code");
+    }
+
+    // O(n^2)
+    // Time Limit Exceeded
+    // Способ глупого квадратичного перебора
+    public int[] TwoSumV1(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            var firstNumber = numbers[i];
+
+            for (int j = 0; j < i; j++)
+            {
+                if (firstNumber + numbers[j] == target && i != j)
+                {
+                    return new[] {i + 1, j + 1};
+                }
+            }
+
+            for (int j = i; j < numbers.Length; j++)
+            {
+                if (firstNumber + numbers[j] == target && i != j)
+                {
+                    return new[] {i + 1, j + 1};
+                }
+            }
+        }
+
+        throw new Exception("Unreachable code");
+    }
+}
