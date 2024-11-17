49f94ee7a0732c69db7f7166c90a99928ff80863
ruslanen <ruslan68a@gmail.com>
2024-11-17 11:30:49 +0300

    27. Remove Element.

---
+++ LeetCode._27_Remove_Element/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._27_Remove_Element;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.RemoveElement(new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }, 2);
+        var result2 = solution.RemoveElement(new int[] { 3, 2, 2, 3 }, 3);
+        var result3 = solution.RemoveElement(new int[] { 2 }, 2);
+        var result4 = solution.RemoveElement(new int[] { 5,2 }, 2);
+        var result5 = solution.RemoveElement(new int[] { 2,5 }, 2);
+        var result6 = solution.RemoveElement(new int[] { 2 }, 3);
+        var result7 = solution.RemoveElement(new int[] { 3,3 }, 3);
+        var result8 = solution.RemoveElement(new int[] { 3,3 }, 5);
+    }
+}
---
+++ LeetCode._27_Remove_Element/Solution.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode._27_Remove_Element;
+
+public class Solution
+{
+    // Решение, в котором имеет значение сохранение целостности массива
+    public int RemoveElement(int[] nums, int val)
+    {
+        if (nums.Length == 0)
+            return 0;
+        
+        var result = 0;
+        var lastIndex = nums.Length - 1;
+    
+        for (int i = 0; i <= lastIndex; i++)
+        {
+            if (nums[i] != val)
+            {
+                result++;
+            }
+        }
+        
+        for (int i = 0; i <= lastIndex; i++)
+        {
+            for (int j = lastIndex; j >= 0; j--)
+            {
+                if (j > i && nums[i] == val)
+                {
+                    if (nums[j] != val)
+                    {
+                        // swap
+                        nums[i] = nums[j];
+                        nums[j] = val;
+                        lastIndex = j;
+                    }
+                }
+            }
+        }
+    
+        return result;
+    }
+    
+    // Правильное решение (It does not matter what you leave beyond the returned k (hence they are underscores).
+    // public int RemoveElement(int[] nums, int val)
+    // {
+    //     int j = 0;
+    //     
+    //     for (int i = 0; i < nums.Length; i++) {
+    //         if (nums[i] != val) {
+    //             nums[j] = nums[i];
+    //             j++;
+    //         }
+    //     }
+    //     
+    //     return j;
+    // }
+}
