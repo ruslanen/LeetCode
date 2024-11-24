d91c6922f7fe3b1281111e07796bdd87a78fbd80
ruslanen <ruslan68a@gmail.com>
2024-11-24 10:40:41 +0300

    228. Summary Ranges.

---
+++ LeetCode._228_Summary_Ranges/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode._228_Summary_Ranges;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.SummaryRanges(new[] { 0, 1, 2, 4, 5, 7 });
+        var result2 = solution.SummaryRanges(new[] { 0, 2, 3, 4, 6, 8, 9 });
+        var result3 = solution.SummaryRanges(new[] { 0 });
+        var result4 = solution.SummaryRanges(new int[] { });
+    }
+}
---
+++ LeetCode._228_Summary_Ranges/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._228_Summary_Ranges;
+
+public class Solution
+{
+    public IList<string> SummaryRanges(int[] nums)
+    {
+        if (!nums.Any())
+            return new List<string>();
+        
+        var rangeStart = nums[0];
+        var ranges = new Dictionary<int, int> { { rangeStart, rangeStart } };
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            var num = nums[i];
+            var nextNum = nums[i + 1];
+            if (num + 1 != nextNum)
+            {
+                rangeStart = nextNum;
+            } 
+            
+            ranges[rangeStart] = nextNum;
+        }
+
+
+        return ranges.Select(x => x.Key == x.Value ? $"{x.Key}" : $"{x.Key}->{x.Value}").ToList();
+    }
+}
