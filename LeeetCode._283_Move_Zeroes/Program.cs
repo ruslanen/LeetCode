89137f36da49c98fee997c09e44ee1314561c5f0
ruslanen <ruslan68a@gmail.com>
2024-12-01 15:08:54 +0300

    283. Move Zeroes.

---
+++ LeeetCode._283_Move_Zeroes/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode._283_Move_Zeroes;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        solution.MoveZeroes(new int[] { 0, 1, 0, 3, 12 });
+        solution.MoveZeroes(new int[] { 0 });
+    }
+}
---
+++ LeeetCode._283_Move_Zeroes/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode._283_Move_Zeroes;
+
+public class Solution
+{
+    // public void MoveZeroes(int[] nums)
+    // {
+    //     for (int i = 0; i < nums.Length; i++)
+    //     {
+    //         for (int j = i + 1; j < nums.Length; j++)
+    //         {
+    //             if (nums[i] == 0)
+    //             {
+    //                 nums[i] = nums[j];
+    //                 nums[j] = 0;
+    //             }
+    //         }
+    //     }
+    // }
+
+    public void MoveZeroes(int[] nums)
+    {
+        int left = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            if (nums[right] != 0)
+            {
+                (nums[left], nums[right]) = (nums[right], nums[left]);
+                left++;
+            }
+        }
+    }
+}
