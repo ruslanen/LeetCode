b9e6f2c25ed5ade2cfa7bfd8b8faa3dfdb4e033b
ruslanen <ruslan68a@gmail.com>
2025-01-01 16:21:13 +0300

    1071. Greatest Common Divisor of Strings.

---
+++ LeetCode._1071_Greatest_Common_Divisor_of_Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode._1071_Greatest_Common_Divisor_of_Strings;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.GcdOfStrings("ABCABCABC", "ABC");
+        var result2 = solution.GcdOfStrings("ABABAB", "ABAB");
+        var result3 = solution.GcdOfStrings("LEET", "CODE");
+        var result4 = solution.GcdOfStrings("AAAAAA", "A");
+        var result5 = solution.GcdOfStrings("ABAB", "ABA");
+        var result6 = solution.GcdOfStrings("ABAB", "ABAC");
+        var result7 = solution.GcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXX");
+    }
+}
---
+++ LeetCode._1071_Greatest_Common_Divisor_of_Strings/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode._1071_Greatest_Common_Divisor_of_Strings;
+
+public class Solution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        if (str1 + str2 != str2 + str1)
+            return string.Empty;
+
+        int gcdLength = Gcd(str1.Length, str2.Length);
+
+        return str1.Substring(0, gcdLength);
+    }
+
+    private int Gcd(int a, int b)
+    {
+        // Алгоритм Евклида для НОД
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
