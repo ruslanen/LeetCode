242707207db5c5ec94a7afdb76d2ff98e2dd35ff
ruslanen <ruslan68a@gmail.com>
2025-01-03 14:08:51 +0300

    605. Can Place Flowers.

---
+++ LeetCode._605_Can_Place_Flowers/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._605_Can_Place_Flowers;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.CanPlaceFlowers(new[] { 1, 0, 0, 0, 1 }, 1);
+        var result2 = solution.CanPlaceFlowers(new[] { 1, 0, 0, 0, 1 }, 2);
+        var result3 = solution.CanPlaceFlowers(new[] { 1 }, 1);
+        var result4 = solution.CanPlaceFlowers(new[] { 0, 0, 0, 0 }, 1);
+        var result5 = solution.CanPlaceFlowers(new[] { 0, 0, 1, 0, 1 }, 1);
+        var result6 = solution.CanPlaceFlowers(new[] { 1, 0, 0, 0, 1, 0, 0 }, 2);
+        var result7 = solution.CanPlaceFlowers(new[] { 0 }, 1);
+        var result8 = solution.CanPlaceFlowers(new[] { 0, 1, 0 }, 1);
+    }
+}
---
+++ LeetCode._605_Can_Place_Flowers/Solution.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+
+namespace LeetCode._605_Can_Place_Flowers;
+
+public class Solution
+{
+    // public bool CanPlaceFlowers(int[] flowerbed, int n)
+    // {
+    //     if (flowerbed.Length == 1 && flowerbed[0] == 0)
+    //         return --n <= 0;
+    //
+    //     if (flowerbed.Length == 1 && flowerbed[0] == 1)
+    //         return n <= 0;
+    //     
+    //     for (int i = 0; i < flowerbed.Length; i++)
+    //     {
+    //         if (flowerbed[i] == 1)
+    //             continue;
+    //         
+    //         if (i == 0 && flowerbed[i + 1] == 0)
+    //         {
+    //             n--;
+    //             flowerbed[i] = 1;
+    //         }
+    //         else if (i == flowerbed.Length - 1 && flowerbed[i-1] == 0)
+    //         {
+    //             n--;
+    //             flowerbed[i] = 1;
+    //         }
+    //         else if (i != 0 && flowerbed[i-1] == 0 && flowerbed[i+1] == 0)
+    //         {
+    //             n--;
+    //             flowerbed[i] = 1;
+    //         }
+    //         
+    //     }
+    //
+    //     return n <= 0;
+    // }
+
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        for (int i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 0
+                && (i == 0 || flowerbed[i - 1] == 0)
+                && (i == flowerbed.Length - 1 || flowerbed[i + 1] == 0))
+            {
+                n--;
+                i += 2;
+            }
+            else
+            {
+                i++;
+            }
+        }
+
+        return n <= 0;
+    }
+}
