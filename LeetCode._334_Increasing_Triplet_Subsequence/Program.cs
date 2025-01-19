e1a4d0ffc643279e7caf3183d45cba571ef89eab
ruslanen <ruslan68a@gmail.com>
2025-01-19 12:39:14 +0300

    334. Increasing Triplet Subsequence.

---
+++ LeetCode._334_Increasing_Triplet_Subsequence/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode._334_Increasing_Triplet_Subsequence;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.IncreasingTriplet(new[] { 1, 2, 3, 4, 5 });
+        var result2 = solution.IncreasingTriplet(new[] { 5, 4, 3, 2, 1 });
+        var result3 = solution.IncreasingTriplet(new[] { 2, 1, 5, 0, 4, 6 });
+        var result4 = solution.IncreasingTriplet(new[] { 0, -1, -2, -3, 1, 4, -1 });
+        var result5 = solution.IncreasingTriplet(new[] { 0 });
+        var result6 = solution.IncreasingTriplet(new[] { -3, -2, -1 });
+        var result7 = solution.IncreasingTriplet(new[] { 20, 100, 10, 12, 5, 13 });
+        var result8 = solution.IncreasingTriplet(new[] { 20, 100, 10, 120, 5, 13, 6, 18 });
+        var result9 = solution.IncreasingTriplet(new[] { 1, 5, 0, 4, 1, 3 });
+    }
+}
---
+++ LeetCode._334_Increasing_Triplet_Subsequence/Solution.cs
@@ -0,0 +1,73 @@
+﻿namespace LeetCode._334_Increasing_Triplet_Subsequence;
+
+public class Solution
+{
+    /// <summary>
+    /// Алгоритм на основе двух барьеров
+    /// </summary>
+    public bool IncreasingTriplet(int[] nums)
+    {
+        if (nums.Length < 3)
+            return false;
+
+        var first = Int32.MaxValue;
+        var second = Int32.MaxValue;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] < first)
+            {
+                first = nums[i];
+            }
+            else if (nums[i] > first && nums[i] < second)
+            {
+                second = nums[i];
+            }
+            else if (nums[i] > second)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    /*
+     * ░░░░░░░░░░░░░░░░░░░░░░░▄▄▄▄▄▄░░░░░░░░░░
+       ░░░░░░░░░░░░░░░░█▀█▄▄▀▀░░░░░░▀█▄░░░░░░░
+       ░░░░░░░░░░░█▀▀▀▀░░░░░░░░░░░░░░░░▀▄▄▄▄░░
+       ░░░░░░░░░▄▄▀░░░▄█░░░░░░░░░░░░░░░░▄▄██▄░
+       ░░░░░░░░░█░░▄█▀░░▀▀▀▀▀▄▄░░░░░░░▄▄█░▀█░░
+       ░░░░░░░░░█░▄▀░░░░░░░░░░░▀▀▀▀▄▄███░░░▀▄░
+       ░░░░░░░░░▀▄█▄███▄▄░░░▄▄▄▄░░░░▄▀░░░░░░█░
+       ░░░░░░░░░░█▄█▀██▀░░░░█▀▄▄▀░░█░░░░░░░█▀░
+       ░░░░░░░░░██░██▀░░░░░▄█▄███░░█░░░░░░░█░░
+       ░░░░░░░▄▄█▀█▄░░▄▄▄▄░░█░░░░▄▀░░░░░░░█░░░
+       ░░░░░▄▀░░███▄░░▀▀░▀░░░░░▄█▄░░░░░░░█▀░░░
+       ░░░▄▀░░░▄█░█▄▄▄▄▄▄▄░░░░░█▀███░░░░░█░░░░
+       ░░▄▀░░░░░██████▀▀▀█░░░░░░██░█▄▄▄▄▄█░░░░
+       ░█▀░░░░░░██░▄██▀▀██░█░░░▄▀██▀░░░░▀▀▀▄░░
+       ▀▀░░░░░░░█▀██░░▄███░█░░░█▀░░░░░░░░░░░█▄
+       ░░░░░░░░░░▄█▀▀▀██▀░░░░▄▀░░░░░░░░░░░░░▀▀
+       ░░░░░░░░░░▀▄░░░░░░░░▄▀░░░░░░░░░░░░░░░░░
+       ░░░░░░░░░░░░█▄▀▀▀▄▀▀░░░░░░░░░░░░░░░░░░░
+     */
+    // public bool IncreasingTriplet(int[] nums)
+    // {
+    //     for (int i = 0; i < nums.Length; i++)
+    //     {
+    //         for (int j = i + 1; j < nums.Length; j++)
+    //         {
+    //             for (int k = j + 1; k < nums.Length; k++)
+    //             {
+    //                 if (nums[i] < nums[j] && nums[j] < nums[k])
+    //                 {
+    //                     Console.WriteLine($"{nums[i]}<{nums[j]}<{nums[k]}");
+    //                     return true;
+    //                 }
+    //             }
+    //         }
+    //     }
+    //
+    //     return false;
+    // }
+}
