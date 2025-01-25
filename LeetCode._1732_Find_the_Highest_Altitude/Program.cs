686d841b2fb6d670d16cbb6c8c7bde7ad08862c9
ruslanen <ruslan68a@gmail.com>
2025-01-25 19:44:48 +0300

    1732. Find the Highest Altitude.

---
+++ LeetCode._1732_Find_the_Highest_Altitude/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LeetCode._1732_Find_the_Highest_Altitude;
+
+public class Program
+{
+    public static void Main()
+    {
+        var solution = new Solution();
+        var result1 = solution.LargestAltitude(new int[] { -5, 1, 5, 0, -7 });
+        var result2 = solution.LargestAltitude(new int[] { -4, -3, -2, -1, 4, 3, 2 });
+        var result3 = solution.LargestAltitude(new int[] { -4, 0, 100 });
+        var result4 = solution.LargestAltitude(new int[] { 1 });
+    }
+}
---
+++ LeetCode._1732_Find_the_Highest_Altitude/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._1732_Find_the_Highest_Altitude;
+
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        var cumulative = 0;
+        var max = 0;
+        for (int i = 0; i < gain.Length; i++)
+        {
+            cumulative += gain[i];
+            max = Math.Max(max, cumulative);
+        }
+
+        return max;
+    }
+}
