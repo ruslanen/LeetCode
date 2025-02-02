3906c6d83d69017cf2d45b80d5f920e76555d10e
ruslanen <ruslan68a@gmail.com>
2025-02-02 09:27:57 +0300

    11. Container With Most Water.

--- LeetCode._11_Container_With_Most_Water/Program.cs
+++ LeetCode._11_Container_With_Most_Water/Program.cs
@@ -6,5 +6,8 @@ public class Program
     {
         var solution = new Solution();
         var result1 = solution.MaxArea(new[] {1, 8, 6, 2, 5, 4, 8, 3, 7});
+        var result2 = solution.MaxArea(new[] {1,1});
+        var result3 = solution.MaxArea(new[] {1,1,2});
+        var result4 = solution.MaxArea(new[] {1,2,4,3});
     }
 }
--- LeetCode._11_Container_With_Most_Water/Solution.cs
+++ LeetCode._11_Container_With_Most_Water/Solution.cs
@@ -2,59 +2,27 @@ namespace LeetCode._11_Container_With_Most_Water;
 
 public class Solution
 {
-    // O(n)
     public int MaxArea(int[] height)
     {
-        // В основе алгоритма лежат два указателя - левый и правый.
-        // Изначально левый - первый элемент.
-        // правый - последний.
-        // Алгоритм будет увеличивать левый указатель в случае, если правая высота больше, чем левая
-        // или увеличивать правый указатель в случае, если левая высота больше, чем правая.
-        // Здесь же, сразу будет вычислять площадь и максимум.
-        // Алгоритм на каждом шаге будет работать только с максимальным левым или правым указателем,
-        // что позволит не выполнять лишние вычисления.
-        var li = 0;
-        var ri = height.Length - 1;
-        var max = 0;
-        while (li != height.Length - 1 && ri != 0)
+        var maxArea = 0;
+        var left = 0;
+        var right = height.Length - 1;
+
+        while (left < right)
         {
-            var left = height[li];
-            var right = height[ri];
-            var val = Math.Min(left, right) * (ri - li);
-
-            if (val > max)
-                max = val;
-
-            if (right > left)
+            var minHeight = Math.Min(height[left], height[right]);
+            var width = right - left;
+            maxArea = Math.Max(maxArea, minHeight * width);
+            if (height[left] > height[right])
             {
-                li++;
+                right--;
             }
             else
             {
-                ri--;
+                left++;
             }
         }
 
-        return max;
-    }
-    
-    // O(n^2)
-    // Time Limit Exceeded
-    public int MaxAreaV1(int[] height)
-    {
-        var max = 0;
-        for (int i = 0; i < height.Length; i++)
-        {
-            for (int j = i; j < height.Length; j++)
-            {
-                var value = Math.Min(height[i], height[j]) * (j - i);
-                if (value > max)
-                {
-                    max = value;
-                }
-            }
-        }
-
-        return max;
+        return maxArea;
     }
 }
